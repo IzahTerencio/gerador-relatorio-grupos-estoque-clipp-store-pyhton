4bd414fe69253b63bd0c346f5d137316031c82a1
Izabella <izahterencio@gmail.com>
2023-09-11 17:58:23 -0300

    Commit teste

---
+++ app_relatorio/main.py
@@ -0,0 +1,36 @@
+'''
+    Filename: main.py
+    Author: izahterencio@gmail.com
+    Date: 2023/09/04
+'''
+
+
+# buscar faixa de data
+# realizar consulta
+# exibir o resultado
+
+
+def generate_report(period):
+    print("Hi")
+
+# realiza a query
+# acessa o banco de dados
+# exibe o resultado ao usuário
+
+
+def start():
+    print('Bem-vindo!\n' +
+          'Informe o período de abrangência do relatório')
+
+    first_day = input('Data inicial: ')
+    last_day = input('Data final: ')
+
+    # manipula formato de data
+    # retorna período
+
+    # chama função gerar relatório passando a data
+
+
+# Call the user interactive  function
+if __name__ == '__main__':
+    start()
