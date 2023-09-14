765a6c0aedae58aa3f39c0fe64249b198e436745
Izabella <izahterencio@gmail.com>
2023-09-14 15:43:56 -0300

    Adding query and date manipulation

--- app_relatorio/main.py
+++ app_relatorio/main.py
@@ -5,28 +5,50 @@
 '''
 
 
-# buscar faixa de data
-# realizar consulta
-# exibir o resultado
+'''Função responsável por realizar a conexão com o banco de dados do sistema'''
+def connect_database(name, usr, passwd):
 
 
-def generate_report(period):
-    print("Hi")
+def generate_report(start, end):
+    query = ("SELECT GRUPO.descricao AS GRUPO, "
+             "SUM((ITEM.vlr_total + ITEM.vlr_despesa - ITEM.vlr_desc)) AS VAL_VENDIDO "
+             "FROM tb_est_grupo AS GRUPO, tb_estoque AS ESTOQUE, tb_nfv_item AS ITEM, "
+             "tb_est_identificador AS ESTOQUE_ID, tb_nfvenda AS VENDA "
+             "WHERE GRUPO.id_grupo IS NOT NULL AND GRUPO.id_grupo = ESTOQUE.id_grupo AND ESTOQUE.id_estoque = "
+             "ESTOQUE_ID.id_estoque AND ESTOQUE_ID.id_identificador = ITEM.id_identificador AND ITEM.id_nfvenda = "
+             "VENDA.id_nfvenda AND VENDA.dt_emissao BETWEEN " + start + " AND " + end + " GROUP BY GRUPO.descricao")
 
 # realiza a query
 # acessa o banco de dados
 # exibe o resultado ao usuário
 
 
+'''Recebe entrada do usuário e padroniza a mesma'''
+def format_string_date(day):
+    aux = ''
+
+    for i in day:
+        if(i != '/'):
+            aux += i
+        else:
+            aux += '-'
+
+    return(aux)
+
+
+'''Interface de interação com o usuário'''
 def start():
     print('Bem-vindo!\n' +
           'Informe o período de abrangência do relatório')
 
     first_day = input('Data inicial: ')
-    last_day = input('Data final: ')
+    first_day = format_string_date(first_day)
 
-    # manipula formato de data
-    # retorna período
+    last_day = input('Data final: ')
+    last_day = format_string_date(last_day)
+
+    generate_report(first_day, last_day)
+
 
     # chama função gerar relatório passando a data
 
